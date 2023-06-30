3f50ecb826f15216c4ea5e5520257a9ca74ec64c
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-06-29 19:35:19 -0700

    Add Boolean Logic Assignment

---
+++ Boolean Logic Assignment/Boolean Logic Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        //Get user info
+        Console.WriteLine("What is your age?");
+        string userAgeString = Console.ReadLine();
+        int userAge = Convert.ToInt32(userAgeString);
+
+        Console.WriteLine("Have you ever had a DUI? Please write \"True\" or \"False\"");
+        string userDUIString = Console.ReadLine();
+        bool userDUI = Convert.ToBoolean(userDUIString);
+
+        Console.WriteLine("How many speeding tickets do you have?");
+        string userSpeedingTicketsString = Console.ReadLine();
+        int userSpeedingTickets = Convert.ToInt32(userSpeedingTicketsString);
+
+        //Check validity of user
+        bool validAge = (userAge > 15);
+        bool validDUI = (userDUI == false);
+        bool validSpeedingTickets = (userSpeedingTickets <= 3);
+
+        //Print result of validity check
+        bool userValid = (validAge && validDUI && validSpeedingTickets);
+        Console.WriteLine("Qualified?");
+        Console.WriteLine(userValid);
+
+        Console.Read();
+        }
+    }
+
