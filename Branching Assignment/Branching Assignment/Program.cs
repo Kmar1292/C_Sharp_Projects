9226ab5a784aca2c2cb09be4c832abae15bb1f8b
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-06-30 17:17:14 -0700

    Nest if else statements

--- Branching Assignment/Branching Assignment/Program.cs
+++ Branching Assignment/Branching Assignment/Program.cs
@@ -14,31 +14,35 @@ class Program
         //Make sure package weight is less than 50
         if (package_weight > 50)
         {
-            Console.WriteLine("Package too heavy to be shipoped via Package Express. Have a good day.");
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             Console.Read();
         }
-
-        //Get package dimensions
-        Console.WriteLine("Enter package width: ");
-        float package_width = Convert.ToSingle(Console.ReadLine());
-        Console.WriteLine("Enter package height: ");
-        float package_height = Convert.ToSingle(Console.ReadLine());
-        Console.WriteLine("Enter package length: ");
-        float package_length = Convert.ToSingle(Console.ReadLine());
-
-        //Make sure sum of dimensions is less than 50
-        double package_sum_dimensions = package_width + package_height + package_length;
-        if (package_sum_dimensions > 50)
+        else
         {
-            Console.WriteLine("Package too big to be shipped via Package Express.");
-            Console.Read();
+            //Get package dimensions
+            Console.WriteLine("Enter package width: ");
+            float package_width = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter package height: ");
+            float package_height = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter package length: ");
+            float package_length = Convert.ToSingle(Console.ReadLine());
+
+            //Make sure sum of dimensions is less than 50
+            double package_sum_dimensions = package_width + package_height + package_length;
+            if (package_sum_dimensions > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.Read();
+            }
+            else
+            {
+                //Calculate and display quote
+                double package_quote = package_width * package_length * package_height * package_weight / 100.00;
+                Console.WriteLine("Your estimated total for shipping this package is: $" + Math.Round(package_quote, 2, MidpointRounding.AwayFromZero).ToString("0.00"));
+
+
+                Console.Read();
+            }
         }
-
-        //Calculate and display quote
-        double package_quote = package_width * package_length * package_height * package_weight / 100.00;
-        Console.WriteLine("Your estimated total for shipping this package is: $" + Math.Round(package_quote, 2, MidpointRounding.AwayFromZero).ToString("0.00"));
-
-
-        Console.Read();
     }
 }
