27c3896bfbe4602472e73f3f7acc463ead4c103c
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-07-04 15:40:05 -0700

    Add TwentyOne project and Calling Methods assignment

---
+++ Calling Methods Assignment/Calling Methods Assignment/MathOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class MathOperations
+    {
+        //Add 42
+        public static int Add42(int number)
+        {
+            int result = number + 42;
+            return result;
+        }
+
+        //Multiply by 3
+        public static int Multiply3(int number)
+        {
+            int result = number * 3;
+            return result;
+        }
+
+        //Modulus 2
+        public static int Modulus2(int number)
+        {
+            int result = number % 2;
+            return result;
+        }
+
+
+
+    }
+}
---
+++ Calling Methods Assignment/Calling Methods Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter an integer: ");
+                int UserInput = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(UserInput + " + 42 equals " + MathOperations.Add42(UserInput));
+                Console.WriteLine(UserInput + " * 3 equals " + MathOperations.Multiply3(UserInput));
+                Console.WriteLine(UserInput + " % 2 equals " + MathOperations.Modulus2(UserInput));
+
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+            
+
+
+        }
+    }
+}
