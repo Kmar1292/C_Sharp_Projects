78e9ad4c8c9f4fbdd4b07e0e5f996cd222e4e346
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-07-05 22:20:59 -0700

    Add Method Assignment

---
+++ Method Assignment/Method Assignment/MathOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class MathOperations
+    {
+        public int Operations(int num1, int num2 = 0)
+        {
+            int result = (num1 / 2) + (num2 * 2);
+            return result;
+        }
+
+    }
+}
---
+++ Method Assignment/Method Assignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate MathOperations obj
+            MathOperations ops = new MathOperations();
+            Console.WriteLine("Enter an integer: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            //Ask user for optional int. If input is not an integer, the format exception will direct the program to only pass 1 arg into the Operations method
+            try
+            {
+                Console.WriteLine("Enter another optional integer: ");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("(" + number1 + " / 2) + (" + number2 + " * 2) = " + ops.Operations(number1, number2));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(number1 + " / 2 = " + ops.Operations(number1));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
