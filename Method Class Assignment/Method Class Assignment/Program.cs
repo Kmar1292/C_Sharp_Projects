0a1419f84cf05c95ffee662a573e2c892800ec23
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-07-05 22:29:28 -0700

    Add Method Class Assignment

---
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating VoidMethod obj
+            VoidMethod testing = new VoidMethod();
+            Console.WriteLine("Input an integer: ");
+            int input1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input another integer: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            testing.test(num1: input1, num2: input2);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Method Class Assignment/Method Class Assignment/VoidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class VoidMethod
+    {
+        public void test(int num1, int num2)
+        {
+            int result = num1 * 2;
+            Console.WriteLine(num1 + " * 2 = " + result);
+            Console.WriteLine(num2);
+        }
+    }
+}
