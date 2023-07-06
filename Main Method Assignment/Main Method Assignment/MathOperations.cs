a80c3833ded67d4cc83047e8ad9481e5a38ff128
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-07-05 18:51:13 -0700

    Add Main Method assignment

---
+++ Main Method Assignment/Main Method Assignment/MathOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class MathOperations
+    {
+        public int Operation(int num)
+        {
+            int result = num + 12;
+            return result;
+        }
+
+        public int Operation(float num)
+        {
+            float result = num / 3;
+            return (int)result;
+        }
+
+        public int Operation(string stringInput)
+        {
+            int result = Convert.ToInt32(stringInput) * 4;
+            return result;
+        }
+
+    }
+}
---
+++ Main Method Assignment/Main Method Assignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate MathOperations
+            MathOperations mathOperations = new MathOperations();
+
+            //Have user input integer and pass integer into Operation method
+            Console.WriteLine("Enter an integer: ");
+            int intInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(intInput + " + 12 equals " + mathOperations.Operation(intInput));
+
+            //Have user input float and pass float into Operation method
+            Console.WriteLine("Enter a decimal number: ");
+            float floatInput = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(floatInput + " / 3 equals " + mathOperations.Operation(floatInput));
+
+            //Have user input integer and pass STRING into Operation method
+            Console.WriteLine("Enter an integer: ");
+            string stringInput = Console.ReadLine();
+            try
+            {
+                Console.WriteLine(stringInput + " times 4 equals " + mathOperations.Operation(stringInput));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
