4b6f29c67cc5df40e969fcf587df769b2ad36d93
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-07-07 22:35:08 -0700

    Add parameters assignment

---
+++ Parameters Assignment/Parameters Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.things = new List<string>();
+            employee.things.Add("car");
+            employee.things.Add("bike");
+            employee.things.Add("toilet");
+
+            Employee<int> employeeTwo = new Employee<int>();
+            employeeTwo.things = new List<int>();
+            employeeTwo.things.Add(3);
+            employeeTwo.things.Add(12);
+            employeeTwo.things.Add(42);
+
+            foreach (string thing in employee.things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (int thing in employeeTwo.things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.Read();
+        }
+    }
+}
