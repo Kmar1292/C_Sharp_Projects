3cc932ac6abaa20d8f8036aa6c9f46e721de70df
Kmar1292 <113205586+Kmar1292@users.noreply.github.com>
2023-07-20 00:04:26 -0700

    Add datetime assignment

---
+++ DateTime Assignment/DateTime Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Input a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The time in {0} hours will be: " + DateTime.Now.AddHours(number), number);
+
+            Console.Read();
+        }
+    }
+}
